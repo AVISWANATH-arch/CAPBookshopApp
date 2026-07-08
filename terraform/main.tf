a8750be7cc37f0a468876899ad212b3d84af5209
Archana Vishwanath <archana.vishwanath@igen43.com>
2026-07-08 17:41:08 +0530

    add terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
+# resource "null_resource" "create_cf_space" {
+
+#   provisioner "local-exec" {
+#     command = "cf create-space ${var.cf_space} -o ${var.cf_org}"
+#   }
+
+# }
 resource "null_resource" "create_cf_space" {
-
   provisioner "local-exec" {
-    command = "cf create-space ${var.cf_space} -o ${var.cf_org}"
+    command = "cf space bookshop-dev || cf create-space bookshop-dev -o org-build-igen43-lab-inhouse-jv4r8a7p"
   }
-
 }
